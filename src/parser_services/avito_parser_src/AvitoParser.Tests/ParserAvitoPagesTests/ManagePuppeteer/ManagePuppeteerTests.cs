e9c3f9f7848857ba7bde3d9a2e749b8367c45fd2
Ramwsh <jimkrauz@gmail.com>
2025-03-06 14:57:45 +0300

    Added some TDD features

--- src/parser_services/avito_parser_src/AvitoParser.Tests/ParserAvitoPagesTests/ManagePuppeteer/ManagePuppeteerTests.cs
+++ src/parser_services/avito_parser_src/AvitoParser.Tests/ParserAvitoPagesTests/ManagePuppeteer/ManagePuppeteerTests.cs
@@ -1,8 +1,9 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Extensions.DependencyInjection;
 using PuppeteerExtraSharp;
 using PuppeteerExtraSharp.Plugins.ExtraStealth;
 using PuppeteerSharp;
 using PuppeteerSharp.BrowserData;
+using RemTechCommon.Utils.ResultPattern;
 using Serilog;
 using ILogger = Serilog.ILogger;
 
@@ -10,23 +11,36 @@ namespace AvitoParser.Tests.ParserAvitoPagesTests.ManagePuppeteer;
 
 public sealed record ScraperOptionsFactory
 {
+    private readonly ILogger _logger;
+    public ScraperOptionsFactory(ILogger logger) => _logger = logger;
+
     public LaunchOptions Create(bool headless = false,
         int width = 1920,
         int height = 1080,
-        int deviceScaleFactor = 1)
+        int deviceScaleFactor = 1,
+        params string[] arguments)
     {
-        ViewPortOptions viewPortOptions = new ViewPortOptions()
+        ViewPortOptions viewPortOptions = new ViewPortOptions
         {
             Width = width,
             Height = height,
-            DeviceScaleFactor = deviceScaleFactor,
+            DeviceScaleFactor = deviceScaleFactor
         };
+
+        _logger.Information(
+            "Scraper view port options has been set: Width: {Width} Height: {Height} Device Scale Factor: {Device Scale Factor}",
+            width, height, deviceScaleFactor);
+
         LaunchOptions launchOptions = new LaunchOptions
         {
             DefaultViewport = viewPortOptions,
-            Headless = headless
+            Headless = headless,
+            Args = arguments
         };
 
+        _logger.Information("Launch options has been set");
+        _logger.Information("Headless {Headless}", headless);
+        _logger.Information("Parameters: {Parameters}", string.Join(' ', arguments));
         return launchOptions;
     }
 }
@@ -34,85 +48,86 @@ public sealed record ScraperOptionsFactory
 public sealed record ScraperInstaller
 {
     private readonly ILogger _logger;
-
     public ScraperInstaller(ILogger logger) => _logger = logger;
 
-    public async Task<string> InstallScraper()
+    public async Task<Result> InstallScraper()
     {
-        _logger.Information("Start downloading Puppeteer.");
+        _logger.Information("{Context}. Start downloading Puppeteer.", nameof(ScraperInstaller));
         BrowserFetcher fetcher = new BrowserFetcher();
-        Task<InstalledBrowser> loadingProcess = fetcher.DownloadAsync();
-        Stopwatch sw = Stopwatch.StartNew();
-        _logger.Information("Puppeteer loading has been started");
-        while (loadingProcess.IsCompleted == false)
-            _logger.Information("Loading puppeteer... {seconds} seconds", sw.Elapsed.Seconds);
-        InstalledBrowser installedBrowser = loadingProcess.Result;
-        string executablePath = installedBrowser.GetExecutablePath();
-        _logger.Information("Puppeteer has loaded browser.");
-        _logger.Information("Executable path: {Path}", executablePath);
-        return executablePath;
+        try
+        {
+            InstalledBrowser browser = await fetcher.DownloadAsync();
+            _logger.Information("{Context}. Puppeteer downloaded. Excutable path: {Path}", nameof(ScraperInstaller),
+                browser.GetExecutablePath());
+            return Result.Success();
+        }
+        catch (Exception ex)
+        {
+            _logger.Information("{Context} failed at downloading Puppeteer. Error: {Message}", nameof(ScraperInstaller),
+                ex.Message);
+            return Result.Failure(new Error("Cannot download puppeteer automatic browser"));
+        }
     }
 }
 
-public sealed record ScraperInstanceFactory(ILogger logger)
+public sealed record ScraperInstanceFactory
 {
-    public async Task<IBrowser> Create()
+    private readonly ScraperInstaller _installer;
+    private readonly ILogger _logger;
+    private readonly ScraperOptionsFactory _optionsFactory;
+    private const string BrowserDirectory = "Chrome";
+
+    public ScraperInstanceFactory(ILogger logger, ScraperInstaller installer, ScraperOptionsFactory factory) =>
+        (_logger, _installer, _optionsFactory) = (logger, installer, factory);
+
+    public async Task<Result<IBrowser>> Create(params string[] arguments)
     {
-        bool exists = Directory.Exists("Chrome");
-        if (exists)
-        {
-            return await Instantiate();
-        }
-        else
-        {
-        }
+        bool exists = Directory.Exists(BrowserDirectory);
+        if (exists) return await Instantiate(arguments);
+
+        _logger.Information("Automatic browser executable does not exist. Downloading.");
+        Result result = await _installer.InstallScraper();
+        if (!result.IsFailure) return await Instantiate(arguments);
+
+        Error error = result.Error;
+        _logger.Error("{Context}. Can't instantiate new browser instance. Error: {error}",
+            nameof(ScraperInstanceFactory), error.Description);
+        return error;
     }
 
-    private async Task<IBrowser> Instantiate()
+    private async Task<Result<IBrowser>> Instantiate(params string[] arguments)
     {
-        var extra = new PuppeteerExtra();
-        var stealth = new StealthPlugin();
-        LaunchOptions
-        extra.Use(stealth);
-        return (await extra.LaunchAsync(options));
+        PuppeteerExtra extra = new PuppeteerExtra();
+        StealthPlugin plugin = new StealthPlugin();
+        extra.Use(plugin);
+        LaunchOptions options = _optionsFactory.Create(arguments: arguments);
+        IBrowser browser = await extra.LaunchAsync(options);
+        _logger.Information("{Context}. Instantiated new browser instance", nameof(ScraperInstanceFactory));
+        return Result<IBrowser>.Success(browser);
     }
 }
 
 public class ManagePuppeteerTests
 {
-    private readonly ILogger _logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+    private readonly IServiceProvider _provider;
 
-    [Fact]
-    public async Task Download_Puppeteer()
+    public ManagePuppeteerTests()
     {
-        BrowserFetcher fetcher = new BrowserFetcher();
-        _logger.Information("Start downloading Puppeteer.");
-        var installed = await fetcher.DownloadAsync();
-        _logger.Information("Finish downloading Puppeteer.");
-        string executablePath = installed.GetExecutablePath();
-        _logger.Information("Executable path: {Path}", executablePath);
-        Assert.NotNull(executablePath);
-        Assert.NotEmpty(executablePath);
-        Assert.True(File.Exists(executablePath));
-        // creates Chrome directory in application folder.
+        IServiceCollection services = new ServiceCollection();
+        services.AddSingleton<ILogger>(new LoggerConfiguration().WriteTo.Console().CreateLogger());
+        services.AddTransient<ScraperOptionsFactory>();
+        services.AddTransient<ScraperInstaller>();
+        services.AddTransient<ScraperInstanceFactory>();
+        _provider = services.BuildServiceProvider();
     }
 
     [Fact]
     public async Task Start_Puppeteer_Browser()
     {
-        var extra = new PuppeteerExtra();
-        var stealth = new StealthPlugin();
-        LaunchOptions options = new LaunchOptions
-        {
-            DefaultViewport = new ViewPortOptions()
-            {
-                Width = 1920,
-                Height = 1080,
-                DeviceScaleFactor = 1,
-            },
-            Headless = false,
-        };
-        await using IBrowser browser = await extra.Use(stealth).LaunchAsync(options);
+        ScraperInstanceFactory factory = _provider.GetRequiredService<ScraperInstanceFactory>();
+        Result<IBrowser> instantiation = await factory.Create();
+        Assert.True(instantiation.IsSuccess);
+        await using IBrowser browser = instantiation.Value;
         var page = await browser.NewPageAsync();
         await Task.Delay(TimeSpan.FromSeconds(10));
         await page.GoToAsync("https://bot.sannysoft.com/");
