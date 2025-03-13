9f4dbfc2e212847f88851ae08f1637856b913c4d
Ramwsh <jimkrauz@gmail.com>
2025-03-14 02:13:21 +0300

    added client abstractions for sinking in db. Requires host abstractions to accept sinking contracts

---
+++ src/parser_services/avito_parser_src/AvitoParser.PDK/Sinking/Models/ScrapedAdvertisementSink.cs
@@ -0,0 +1,59 @@
+﻿using AvitoParser.PDK.Models;
+using AvitoParser.PDK.Models.ValueObjects;
+
+namespace AvitoParser.PDK.Sinking.Models;
+
+public class ScrapedAdvertisementSink
+{
+    public ulong AdvertisementId { get; } = 0;
+    public DateTime AdvertisementDate { get; } = DateTime.MinValue;
+    public string Title { get; } = string.Empty;
+    public string Description { get; } = string.Empty;
+    public string Address { get; } = string.Empty;
+    public string SourceUrl { get; } = String.Empty;
+    public ulong PriceValue { get; } = 0;
+    public string PriceDescription { get; } = string.Empty;
+    public string Publisher { get; } = string.Empty;
+    public string[] PhotoUrls { get; } = [];
+    public ScrapedCharacteristicSink[] Characteristics { get; } = [];
+
+    public ScrapedAdvertisementSink(ScrapedAdvertisement advertisement)
+    {
+        AdvertisementId = advertisement.Id.Id;
+        AdvertisementDate = AdvertisementDate.Date.Date;
+        Title = advertisement.Title.Title;
+        Description = advertisement.Description.Description;
+        Address = advertisement.Address.Address;
+        SourceUrl = advertisement.SourceUrl.SourceUrl;
+        PriceValue = advertisement.Price.Price;
+        PriceDescription = advertisement.Price.Description;
+        Publisher = advertisement.Publisher.Name;
+        PhotoUrls = advertisement.Photos.Select(p => p.SourceUrl).ToArray();
+        Characteristics = advertisement
+            .Characteristics.Select(c => new ScrapedCharacteristicSink(c))
+            .ToArray();
+    }
+
+    public static ScrapedAdvertisementSink[] FromEnumerable(
+        IEnumerable<ScrapedAdvertisement> advertisements
+    ) => advertisements.Select(s => new ScrapedAdvertisementSink(s)).ToArray();
+}
+
+public sealed record ScrapedCharacteristicSink
+{
+    public string Name { get; } = string.Empty;
+    public string Value { get; } = string.Empty;
+
+    public ScrapedCharacteristicSink(ScrapedCharacteristic ctx)
+    {
+        Name = ctx.Name;
+        Value = ctx.Value;
+    }
+}
+
+public sealed record ScrapedFromSink
+{
+    public string Name { get; } = string.Empty;
+
+    public ScrapedFromSink(string name) => Name = name;
+}
---
+++ src/parser_services/avito_parser_src/AvitoParser.PDK/Sinking/Models/SinkScrapedAdvertisementsContract.cs
@@ -0,0 +1,9 @@
+﻿using AvitoParser.PDK.Sinking.Models;
+using Rabbit.RPC.Client.Abstractions;
+
+namespace AvitoParser.PDK.Sink;
+
+public record SinkScrapedAdvertisementsContract(
+    ScrapedAdvertisementSink[] Advertisements,
+    ScrapedFromSink From
+) : IContract;
