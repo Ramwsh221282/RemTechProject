abb014c43540b748a5a4a8536c7b92fc7f8e0608
Ramwsh <jimkrauz@gmail.com>
2025-03-14 22:35:03 +0300

    added data service endpoint

---
+++ src/parser_services/avito_parser_src/AvitoParser.Tests/Avito_Parser_Plugins_Tests/SinkingTests.cs
@@ -0,0 +1,44 @@
+﻿using AvitoParser.PDK.Models;
+using AvitoParser.PDK.Models.ValueObjects;
+using AvitoParser.PDK.Sinking.Configurations;
+using AvitoParser.PDK.Sinking.Models;
+using AvitoParser.PDK.Sinking.RabbitMq;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AvitoParser.Tests.Avito_Parser_Plugins_Tests;
+
+public sealed class SinkingTests
+{
+    private const string filePath = "SINK_CLIENT_CONFIG.json";
+    private readonly RabbitSinker _sinker;
+
+    public SinkingTests()
+    {
+        IServiceCollection services = new ServiceCollection();
+        SinkingClientConfiguration configuration =
+            SinkingClientConfiguration.SinkingClientConfigurationLoader.Load(filePath);
+        services.AddSingleton(configuration);
+        services.AddSingleton<RabbitSinker>();
+        IServiceProvider provider = services.BuildServiceProvider();
+        _sinker = provider.GetRequiredService<RabbitSinker>();
+    }
+
+    [Fact]
+    public async Task Sink_Advertisement_Mok()
+    {
+        ScrapedAdvertisement advertisement = new ScrapedAdvertisement()
+        {
+            Address = ScrapedAddress.Create("test"),
+            Characteristics = [ScrapedCharacteristic.Create("Test", "Test")],
+            Date = ScrapedAdvertisementDate.Default,
+            Description = ScrapedDescription.Create("test"),
+            Id = ScrapedAdvertisementId.Create("123"),
+            Photos = [ScrapedPhotoUrl.Create("http://localhost")],
+            Price = ScrapedPrice.Create("123", "NDS"),
+            Title = ScrapedTitle.Create("test"),
+            Publisher = ScrapedPublisher.Create("test"),
+            SourceUrl = ScrapedSourceUrl.Create("test"),
+        };
+        await _sinker.SinkAdvertisements([advertisement], new ScrapedFromSink("AVITO"));
+    }
+}
