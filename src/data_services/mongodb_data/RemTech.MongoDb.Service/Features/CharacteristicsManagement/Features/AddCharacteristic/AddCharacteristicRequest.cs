1d40adfe2e75734e4319d98fb6b843d98406a592
Ramwsh <jimkrauz@gmail.com>
2025-03-28 03:25:38 +0700

    fixed gitignore

--- src/data_services/mongodb_data/RemTech.MongoDb.Service/Features/CharacteristicsManagement/Features/AddCharacteristic/AddCharacteristicRequest.cs
+++
@@ -1,7 +0,0 @@
-using RemTech.MongoDb.Service.Features.CharacteristicsManagement.Models;
-using RemTechCommon.Utils.CqrsPattern;
-using RemTechCommon.Utils.ResultPattern;
-
-namespace RemTech.MongoDb.Service.Features.CharacteristicsManagement.Features.AddCharacteristic;
-
-public sealed record AddCharacteristicRequest(Characteristic Characteristic) : IRequest<Result>;
