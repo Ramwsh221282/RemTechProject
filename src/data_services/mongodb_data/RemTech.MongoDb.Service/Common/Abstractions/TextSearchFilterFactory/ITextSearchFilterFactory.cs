65c84c5deecffbdae42c2020d57457b9ca7aa0af
Ramwsh <jimkrauz@gmail.com>
2025-03-28 03:22:17 +0700

    gitignore cache restored

--- src/data_services/mongodb_data/RemTech.MongoDb.Service/Common/Abstractions/TextSearchFilterFactory/ITextSearchFilterFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using MongoDB.Driver;
-using RemTech.MongoDb.Service.Common.Dtos;
-
-namespace RemTech.MongoDb.Service.Common.Abstractions.TextSearchFilterFactory;
-
-public interface ITextSearchFilterFactory<TFilterEntity> where TFilterEntity : class
-{
-    public FilterDefinition<TFilterEntity> Apply(FilterDefinition<TFilterEntity> filter, TextSearchOption option);
-}
--- src/data_services/mongodb_data/RemTech.MongoDb.Service/Common/Attributes/IndexModelAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace RemTech.MongoDb.Service.Common.Attributes;
-
-[AttributeUsage(AttributeTargets.Class)]
-public sealed class IndexModelAttribute : Attribute { }
-
-[AttributeUsage(AttributeTargets.Method)]
-public sealed class IndexModelMethodAttribute : Attribute { }
